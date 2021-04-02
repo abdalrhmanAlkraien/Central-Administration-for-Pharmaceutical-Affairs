fbe87481abf456524c21bd44f0b2e99ca6cd5d1e
abdalrhman <abdal-rahman@altshiftcreative.com>
2021-04-02 10:58:20 +0300

    init project

---
+++ src/main/java/com/CentralAdministrationForPharmaceuticalAffairs/CentralAdministrationForPharmaceuticalAffairs/CentralAdministrationForPharmaceuticalAffairsApplication.java
@@ -0,0 +1,13 @@
+package com.CentralAdministrationForPharmaceuticalAffairs.CentralAdministrationForPharmaceuticalAffairs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralAdministrationForPharmaceuticalAffairsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralAdministrationForPharmaceuticalAffairsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CentralAdministrationForPharmaceuticalAffairs/CentralAdministrationForPharmaceuticalAffairs/CentralAdministrationForPharmaceuticalAffairsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CentralAdministrationForPharmaceuticalAffairs.CentralAdministrationForPharmaceuticalAffairs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralAdministrationForPharmaceuticalAffairsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
